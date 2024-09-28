9f04431b615d0a9f8de0056ee625d5a09582b158
Fernando Matheus <fernando.matheuss@hotmail.com>
2024-09-28 15:47:37 -0300

    Criando portfólio

--- src/app/pages/public/portfolio/portfolio.component.ts
+++ src/app/pages/public/portfolio/portfolio.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/core/service/app.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css'],
 })
-export class PortfolioComponent {}
+export class PortfolioComponent implements OnInit {
+  projects: any[] = [];
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load() {
+    this.appService.findAll().subscribe(data => {
+      console.log(data); // Verifique no console se os dados estão corretos
+      // Acessando a lista de "projetos" dentro de "portfolio"
+      this.projects = data[0].portfolio[0].projetos;
+    });
+  }
+}
